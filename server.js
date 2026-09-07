abd1662c55cf38ab0d9d7ee32a9b4eb42edc7606
Muhammet Ashirov <mashiryaz@gmail.com>
2026-09-07 14:16:48 +0200

    Allow PUT and DELETE methods in CORS configuration

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || '*');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
